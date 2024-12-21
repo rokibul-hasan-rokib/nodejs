9695d1df8d2e3e0f5a570c31dbdc424b1ae03e4c
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2024-12-21 18:23:02 +0600

    register controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,3 +15,39 @@ export const getAllUsers = async (req, res, next) => {
         })
     }
 }
+
+export const registerController = async(req, res) => {
+    const {name, email, password, phone, address} = req.body;
+    try {
+        const existingUser = await User.findOne({name});
+        if(!existingUser){
+            return res.status(400).json({
+                message: "Username is already here",
+                success: true
+            })
+        }
+        const newUser = new User(req.body);
+        await newUser.save();
+
+        const token = JWT.sign({
+            user: {id: newUser._id, username: newUser.name}
+        }, process.env.JWT_SECRET,
+    {
+        expiresIn: '7d',
+    }
+    );
+    res.status(200).send({
+        success: true,
+        message: "User Register  Successfully",
+        newUser,
+        token,
+    });
+    } catch (error) {
+        console.log(error),
+        res.status(500).json({
+            message: "Internal Server Errro",
+            error,
+        })
+    } 
+   
+}
