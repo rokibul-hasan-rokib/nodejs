6f62291fa65e4cdde4adb964eab45951b9c442af
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2024-12-23 23:58:16 +0600

    product update with image

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -18,10 +18,10 @@ export const getAllProduct = async(req, res) =>{
 }
 
 export const createProduct = async(req,res) => {
-  const {name, description, price, categoryId, image} = req.body;
+  const {name, description, price, categoryId} = req.body;
 
   try {
-     const product = new Product(req.body);
+     const product = new Product(req.body,{image: req.file ? req.file.path : ''});
      const savedProduct = await product.save();
      res.status(200).json({
         success: true,
@@ -36,3 +36,30 @@ export const createProduct = async(req,res) => {
     })
   }
 }
+
+export const updateProduct = async(req, res) => {
+    const updateData = {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        categoryId: req.body.categoryId,
+    };
+    if(req.file){
+        updateData.image = req.file.path;
+    }
+    try {
+        const updateProduct = await Product.findByIdAndUpdate(req.params.id, updateData, {new: true});
+        res.status(200).json({
+            success: true,
+            message: "Product Updated Successfully",
+            updateProduct,
+        })
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            message: "Product Not Upda ted",
+            error,
+        })
+    }
+}
+
