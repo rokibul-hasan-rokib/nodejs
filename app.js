85a39850c8d95250acf51402d250c9e37b91a4f6
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2024-12-28 00:59:09 +0600

    js url and ejs setting

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-import express from 'express'
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
 
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
-app.use('/rokib',function(req, res){
-    res.send('rokib');
+app.get('/welcome/:name',(req,res)=>{
+     res.send(`Welcom to my home ${req.params.name}`)
 })
 
+app.use("/rokib", function (req, res) {
+  res.send("rokib");
+});
 
-app.use('/me',(req,res) =>{
-   res.send("This is me");
+app.get("/ro", function (req, res) {
+  res.send("ro");
+});
+
+app.use("/me", (req, res) => {
+  res.send("This is me");
+});
+
+app.use("/akash", (res, req) => {
+  res.send("rokib");
 });
 
 export default app;
